81644c4317dcd5ab147c4ef33674378efdd905df
lr193 <rashmikaopatha97@gmail.com>
2020-10-12 20:35:05 +1100

    mongo-URI-fix

--- CarParkSystem/api/api.js
+++ CarParkSystem/api/api.js
@@ -1,8 +1,9 @@
 //run npm install nodemon --save-dev
+
 const express = require('express'); //npm install express --save
 
 const mongoose = require('mongoose');   //npm install mongoose
-mongoose.connect( "mongodb+srv://hjayatilleke:hjayatilleke@cluster0.elbtc.mongodb.net/register?retryWrites=true&w=majority", {
+mongoose.connect( "mongodb+srv://hjayatilleke:#########@cluster0.elbtc.mongodb.net/register?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true 
 });
